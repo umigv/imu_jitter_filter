a8cd9fd3de7dbd8372b59f3c8221eb77f73f57ac
umigv-nuc1 <umigv-leads@umich.edu>
2019-02-10 18:28:41 -0500

    adjusted epsilon value and fixed some issues

--- src/imu_jitter_filter.cpp
+++ src/imu_jitter_filter.cpp
@@ -1,35 +1,46 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 #include <cmath>
+#include <iostream>
 
-struct TemporalFilter {
+struct JitterFilter {
 public:
-    explicit TemporalFilter(ros::NodeHandle &node)
+    explicit JitterFilter(ros::NodeHandle &node)
     : last_stamp_{ 0, 0 },
       publisher_{ node.advertise<sensor_msgs::Imu>("imu/data_filtered", 10) }
     { }
 
     void filter(const sensor_msgs::Imu::ConstPtr &imu_ptr) {
-        const auto now = ros::Time::now();
-        const auto stamp = imu_ptr->header.stamp;
+        //const auto now = ros::Time::now();
+        //const auto stamp = imu_ptr->header.stamp;
 
         sensor_msgs::Imu new_data = *imu_ptr;
         static sensor_msgs::Imu prev_data = *imu_ptr;
 
-        const float difference = 10;
+        const float difference = 1;
+	//ROS_INFO("in filter");
 
+	/*
         if (stamp <= last_stamp_ || stamp > now) {
+	    ROS_INFO("stuff2");
             return;
         }
+	*/
 
         bool lin_accel_ok = dataFilter(new_data.linear_acceleration, prev_data.linear_acceleration, difference);
         bool ang_vel_ok = dataFilter(new_data.angular_velocity, prev_data.angular_velocity, difference);
 
-        last_stamp_ = stamp;
+        //last_stamp_ = stamp;
 
         if (lin_accel_ok && ang_vel_ok)
+	{
         	publisher_.publish(new_data);
-
+	}
+	else
+	{
+		std::cout << "did not publish" << std::endl;
+	}
+	
         prev_data = new_data;
     }
 
@@ -38,6 +49,7 @@ private:
     ros::Publisher publisher_;
     bool dataFilter(const auto &data, const auto &prev_data, const float difference)
     {
+	//ROS_INFO("stuff");
     	if (abs(data.x - prev_data.x) > difference)
     	{
     		return false;
@@ -45,7 +57,7 @@ private:
 
     	if (abs(data.y - prev_data.y) > difference)
     	{
-    		return false
+    		return false;
     	}
 
     	if (abs(data.z - prev_data.z) > difference)
@@ -58,13 +70,14 @@ private:
 };
 
 int main(int argc, char *argv[]) {
-    ros::init(argc, argv, "imu_temporal_filter_node");
+    ros::init(argc, argv, "imu_jitter_filter_node");
     ros::NodeHandle node;
+	ROS_INFO("Jitter Filter launched");
 
-    TemporalFilter filter{ node };
+    JitterFilter filter{ node };
     const auto subscriber =
         node.subscribe<sensor_msgs::Imu>("imu/data", 10,
-                                         &TemporalFilter::filter, &filter);
+                                         &JitterFilter::filter, &filter);
 
     ros::spin();
 }
